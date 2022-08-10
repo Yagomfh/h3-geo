e34248da0123a197d6a91e4402635584362abec6
Yago Martinez-Falero Hein <yagomfh@gmail.com>
2022-08-10 11:35:45 +0200

    feat: final code

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
-import React, { useEffect } from 'react';
-import { render } from 'react-dom';
-import { StaticMap, MapContext, NavigationControl } from 'react-map-gl';
-import DeckGL, { GeoJsonLayer, ArcLayer } from 'deck.gl';
+import React, { useEffect, useState } from 'react';
+import { StaticMap, MapContext } from 'react-map-gl';
+import DeckGL, { FlyToInterpolator } from 'deck.gl';
+import { PathLayer } from '@deck.gl/layers';
+import data from './data/source'
+import AddRoundedIcon from '@mui/icons-material/AddRounded';
+import ArrowDropUpIcon from '@mui/icons-material/ArrowDropUp';
+import RemoveRoundedIcon from '@mui/icons-material/RemoveRounded';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import PlayCircleOutlineIcon from '@mui/icons-material/PlayCircleOutline';
+import RotateRightIcon from '@mui/icons-material/RotateRight';
+import RotateLeftIcon from '@mui/icons-material/RotateLeft';
+import ButtonGroup from '@mui/material/ButtonGroup';
+import Typography from '@mui/material/Typography';
+import Card from '@mui/material/Card';
+import Slider from '@mui/material/Slider';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
 
-// source: Natural Earth http://www.naturalearthdata.com/ via geojson.xyz
-const AIR_PORTS =
-  'https://d2ad6b4ur7yvpq.cloudfront.net/naturalearth-3.3.0/ne_10m_airports.geojson';
+const TOKEN = import.meta.env.VITE_MAPBOX_API_KEY || null
 
 const INITIAL_VIEW_STATE = {
   latitude: 51.47,
@@ -15,64 +28,227 @@ const INITIAL_VIEW_STATE = {
   pitch: 30
 };
 
-//const MAP_STYLE = 'https://openmaptiles.github.io/osm-bright-gl-style/style-cdn.json';
-const MAP_STYLE = 'https://basemaps.cartocdn.com/gl/positron-nolabels-gl-style/style.json';
-const NAV_CONTROL_STYLE = {
-  position: 'absolute',
-  top: 10,
-  left: 10
-};
-
 const App = () => {
-  const onClick = info => {
-    if (info.object) {
-      // eslint-disable-next-line
-      alert(`${info.object.properties.name} (${info.object.properties.abbrev})`);
-    }
-  };
+  const [view, setView] = useState(INITIAL_VIEW_STATE)
+  const [layer, setLayer] = useState([])
+  const [progress, setProgress] = useState(100)
+  const [disable, setDisable] = useState(false)
 
-  const layers = [
-    new GeoJsonLayer({
-      id: 'airports',
-      data: AIR_PORTS,
-      // Styles
-      filled: true,
-      pointRadiusMinPixels: 2,
-      pointRadiusScale: 2000,
-      getPointRadius: f => 11 - f.properties.scalerank,
-      getFillColor: [200, 0, 80, 180],
-      // Interactive props
+  const playPath = () => {
+    setDisable(true)
+    for (let i = 0; i <= 100; i++) {
+      setTimeout(() => {
+        setProgress(i)
+        i == 100 && setDisable(false)
+      }, i * 100)
+    }
+  }
+
+  const createLayer = () => {
+    const { geometry: { coordinates } } = data
+    const filteredData = coordinates.slice(0, Math.round(coordinates.length * progress / 100))
+    const l = new PathLayer({
+      id: 'path-layer',
+      data: [{
+        path: filteredData,
+        color: [144, 224, 239]
+      }],
       pickable: true,
-      autoHighlight: true,
-      onClick
-    }),
-    new ArcLayer({
-      id: 'arcs',
-      data: AIR_PORTS,
-      dataTransform: d => d.features.filter(f => f.properties.scalerank < 4),
-      // Styles
-      getSourcePosition: f => [-0.4531566, 51.4709959], // London
-      getTargetPosition: f => f.geometry.coordinates,
-      getSourceColor: [0, 128, 200],
-      getTargetColor: [200, 0, 80],
-      getWidth: 1
+      widthScale: 5,
+      getPath: d => d.path,
+      getColor: d => d.color,
+    });
+    filteredData.length && setView({
+      ...view,
+      latitude: filteredData[filteredData.length - 1][1],
+      longitude: filteredData[filteredData.length - 1][0],
     })
-  ];
+    setLayer(l)
+  }
 
   useEffect(() => {
-    console.log(import.meta)
+    createLayer()
+  }, [progress])
+
+  useEffect(() => {
+    const { geometry: { coordinates } } = data
+    createLayer()
+    setView({
+      ...view,
+      latitude: coordinates[0][1],
+      longitude: coordinates[0][0],
+      zoom: 15,
+      transitionDuration: 5000,
+      transitionInterpolator: new FlyToInterpolator()
+    })
   }, [])
 
   return (
-    <DeckGL
-      initialViewState={INITIAL_VIEW_STATE}
-      controller={true}
-      layers={layers}
-      ContextProvider={MapContext.Provider}
-    >
-      <StaticMap mapStyle={'mapbox://styles/mapbox/satellite-streets-v11'} mapboxApiAccessToken={import.meta.env.VITE_MAPBOX_API_KEY} />
-      <NavigationControl style={NAV_CONTROL_STYLE} />
-    </DeckGL>
+    <Box>
+
+      <DeckGL
+        initialViewState={view}
+        onViewStateChange={({ viewState }) => setView(viewState)}
+        controller={true}
+        layers={[layer]}
+        ContextProvider={MapContext.Provider}
+      >
+        <StaticMap
+          mapStyle={
+            TOKEN ?
+              'mapbox://styles/mapbox/satellite-streets-v11' :
+              'https://basemaps.cartocdn.com/gl/positron-nolabels-gl-style/style.json'
+          }
+          mapboxApiAccessToken={import.meta.env.VITE_MAPBOX_API_KEY}
+        />
+        {/* <NavigationControl style={NAV_CONTROL_STYLE} visualizePitch={true} showCompass={true} /> */}
+        <Box sx={{ margin: 2 }}>
+          <ButtonGroup
+            orientation="vertical"
+            aria-label="vertical outlined button group"
+          >
+
+            <Button
+              variant="contained"
+              color='inherit'
+              sx={{
+                backgroundColor: 'white'
+              }}
+              onClick={() => setView({
+                ...view,
+                zoom: view.zoom + 1,
+                transitionDuration: 0,
+              })}
+            >
+              <AddRoundedIcon sx={{ color: 'black' }} />
+            </Button>
+            <Button
+              variant="contained"
+              color='inherit'
+              sx={{
+                backgroundColor: 'white'
+              }}
+              onClick={() =>
+                setView({
+                  ...view,
+                  zoom: view.zoom - 1,
+                  transitionDuration: 0,
+                })
+              }
+            >
+              <RemoveRoundedIcon sx={{ color: 'black' }} />
+            </Button>
+            <Button
+              variant="contained"
+              color='inherit'
+              sx={{
+                backgroundColor: 'white'
+              }}
+              onClick={() =>
+                setView({
+                  ...view,
+                  pitch: view.pitch + 10,
+                  transitionDuration: 0,
+                })
+              }
+            >
+              <ArrowDropUpIcon sx={{ color: 'black' }} />
+            </Button>
+            <Button
+              variant="contained"
+              color='inherit'
+              sx={{
+                backgroundColor: 'white'
+              }}
+              onClick={() =>
+                setView({
+                  ...view,
+                  pitch: view.pitch - 10,
+                  transitionDuration: 0,
+                })
+              }
+            >
+              <ArrowDropDownIcon sx={{ color: 'black' }} />
+            </Button>
+            <Button
+              variant="contained"
+              color='inherit'
+              sx={{
+                backgroundColor: 'white'
+              }}
+              onClick={() =>
+                setView({
+                  ...view,
+                  bearing: view.bearing + 10,
+                  transitionDuration: 0,
+                })
+              }
+            >
+              <RotateRightIcon sx={{ color: 'black' }} />
+            </Button>
+            <Button
+              variant="contained"
+              color='inherit'
+              sx={{
+                backgroundColor: 'white'
+              }}
+              onClick={() => setView({
+                ...view,
+                bearing: view.bearing - 10,
+                transitionDuration: 0,
+              })
+              }
+            >
+              <RotateLeftIcon sx={{ color: 'black' }} />
+            </Button>
+          </ButtonGroup>
+        </Box>
+      </DeckGL >
+      <Card sx={{ position: 'absolute', bottom: 0, right: 0, left: 0, margin: 2, marginBottom: 6 }}>
+        <CardContent>
+          <Typography variant="body1" gutterBottom>
+            Progress:
+          </Typography>
+          <Box sx={{ display: 'flex', justifyContent: 'space-evenly' }}>
+            <Button
+              variant="text"
+              color='inherit'
+              sx={{
+                backgroundColor: 'white',
+                marginTop: 1
+              }}
+              onClick={playPath}
+            >
+              <PlayCircleOutlineIcon sx={{ color: 'black' }} />
+            </Button>
+            <Slider
+              sx={{ width: '90%', marginRight: 2, marginLeft: 4 }}
+              aria-label="path"
+              value={progress}
+              onChange={({ target: { value } }) => setProgress(value)}
+              valueLabelDisplay="auto"
+              min={0}
+              step={1}
+              max={100}
+              disabled={disable}
+              marks={[{
+                value: 0,
+                label: '0%'
+              },
+              {
+                value: 50,
+                label: '50%'
+              },
+              {
+                value: 100,
+                label: '100%'
+              }
+              ]}
+            />
+          </Box>
+        </CardContent>
+      </Card>
+    </Box>
   );
 }
 
